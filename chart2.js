5d559f984cc11a4d7f4ad076fe7cd4a3ba12fe05
punmanus <132586897+punmanus@users.noreply.github.com>
2023-05-04 21:51:36 +0700

    Add files via upload

---
+++ chart2.js
@@ -0,0 +1,40 @@
+d3.csv('water(3).csv')
+    .then( (data)=>{
+    //console.log(data)
+    var xlable = data.map( (d) =>{
+        return d.Date
+        //console.log(d.Year)
+    })
+
+    var ylable = data.map( (d) =>{
+        return d.cwc
+        //console.log(d.Volumn)
+    })
+    var zlable = data.map( (d) =>{
+      return d.twc
+      //console.log(d.Volumn)
+  })
+  
+
+    var chart = new Chart('lineChart', {
+        type: "line",
+        options: {
+          maintainAspectRatio: false,
+          legend: {
+            display: false
+          },
+        },
+        data: {
+          labels: xlable,
+          tension: 0.5,
+          datasets: [
+            {
+              data: ylable,
+              borderColor: 'rgb(54, 162, 235)',
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              fill: false
+            }
+          ]
+        },
+      });
+});
